ee49a340d40fda48ac2fc25a71180acea7a0632d
jelizasilatei <silateijc90@gmail.com>
2023-03-15 15:22:07 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+/*let multiply;
+num1 = 31;
+num2 = 2;
+multiply = num1 * num2;
+console.log(multiply);
+
+let random;
+console.log(Math.floor(Math.random() *20) + 1);*/
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 36;
+let num4 = 8;
+let mod = num3 % num4;
+
+
+let max = Math.max(1, 3, 5, 17, 19, 20);
+console.log(Math.floor(Math.random() * 10));
